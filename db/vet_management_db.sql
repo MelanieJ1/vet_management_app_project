e94b41c0c9867094867ec2f42eaa7399148b899f
Melanie Jayasinghe <melaniej.1@outlook.com>
2022-08-21 14:58:05 +0100

    adding run sql file

---
+++ db/vet_management_db.sql
@@ -0,0 +1,21 @@
+
+DROP TABLE IF EXISTS animals;
+DROP TABLE IF EXISTS vets;
+
+CREATE TABLE vets (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  patient_list VARCHAR(255)
+);
+
+CREATE TABLE animals (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  date_of_birth VARCHAR(255),
+  animal_type VARCHAR(255),
+  client VARCHAR(255)
+  treatment_notes(255)
+  vet_id INT NOT NULL REFERENCES vets(id)
+);
+
