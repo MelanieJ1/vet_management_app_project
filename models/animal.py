fd50b9eb73130ee625386b558246f4678cb0bd23
Melanie Jayasinghe <melaniej.1@outlook.com>
2022-08-21 14:04:47 +0100

    adding classes

---
+++ models/animal.py
@@ -0,0 +1,10 @@
+
+class Animal:
+
+    def __init__(self, name, date_of_birth, type, client_details, treatment_notes, id = None, ):
+        self.name = name
+        self.date_of_birth = date_of_birth
+        self.type = type
+        self.client_details = client_details
+        self.treatment_notes = treatment_notes
+        self.id = id
