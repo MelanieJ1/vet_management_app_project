91aeb46d27a21a6109fa023c6d98a7ce6bf8c127
Melanie Jayasinghe <melaniej.1@outlook.com>
2022-08-23 16:22:40 +0100

    vet controller update

--- controllers/vet_controller.py
+++ controllers/vet_controller.py
@@ -9,10 +9,15 @@ import repositories.animal_repository as animal_repository
 vets_blueprint = Blueprint("vets", __name__)
 
 @vets_blueprint.route("/vets")
-def show(id):
-    vets = vet_repository.select(id)
-    animals = animal_repository.select_all()
-    return render_template("vets/show.html", animal_list = animals)
+def show_vet():
+    vets = vet_repository.select_all()
+    return render_template("vets/show.html", vets = vets)
+
+# @vets_blueprint.route("/vets")
+# def show(id):
+#     vets = vet_repository.select(id)
+#     animals = animal_repository.select_all()
+#     return render_template("vets/show.html", animal_list = animals)
 
 
 @vets_blueprint.route("/vets", methods = ['POST'])
